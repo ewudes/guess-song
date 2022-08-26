f27b0964387700001386c9a18f9f837e1fedb0c1
ewudes <ewudes@gmail.com>
2022-08-26 16:20:37 +0300

    fix: markup

--- src/game.js
+++
@@ -1,9 +0,0 @@
-export const isArtistAnswerCorrect = (question, userAnswer) => {
-  return userAnswer.artist === question.song.artist;
-};
-
-export const isGenreAnswerCorrect = (question, userAnswer) => {
-  return userAnswer.every((answer, index) => {
-    return answer === (question.answers[index].genre === question.genre);
-  });
-};
